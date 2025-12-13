8c8a934f76c45c866305fb9badebf36640760085
Pippijn van Steenhoven <pip88nl@gmail.com>
2025-12-13 16:17:21 +0000

    fix(client): wrap App in Router to enable use_query_map hook

--- sint-ftl/client/src/main.rs
+++ sint-ftl/client/src/main.rs
@@ -1,10 +1,15 @@
 use leptos::mount::mount_to_body;
 use leptos::prelude::*;
+use leptos_router::components::Router;
 use sint_client::App;
 
 fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).expect("error initializing logger");
 
-    mount_to_body(|| view! { <App /> })
+    mount_to_body(|| view! {
+        <Router>
+            <App />
+        </Router>
+    })
 }
